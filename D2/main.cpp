e08df1b3b01af33eac8e7969f2b9a71ccd5570fd
joaonorthh <joao@northh.de>
2022-01-07 18:00:35 +0100

    Push code examples

---
+++ D2/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Joao Patricio on 07/01/2022.
+// Copyright (c) 2022 northh medical GmbH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello Docker container!" << std::endl;
+    return 0;
+}
